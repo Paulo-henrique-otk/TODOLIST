058726a7060575ea51073facec1974d27a401f87
Paulo <paulohenriquelg3@gmail.com>
2022-11-27 14:19:03 -0300

    assets

--- TODOLIST/resources/views/welcome.blade.php
+++ TODOLIST/resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="{{ asset("/css/app.css")}}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
