f96dfe823e36f71a7296fc6eb0ad0d9002e4524e
Guilhermy Endryk <bokinhalvl1@gmail.com>
2022-12-04 22:37:36 -0300

    test1

--- TODOLIST/resources/views/createUser.blade.php
+++ TODOLIST/resources/views/createUser.blade.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/createUser.css">
     <title>Create User</title>
 </head>
 <body>
-<form action="{{ route("get.user") }}" method="post">
-@csrf
-<input type="text" name="nome" id="">
-<input type="password" name="password" id="">
-<button type="submit">Criar</button>
-</form>
+    
+<div>
+    <form action="{{ route("get.user") }}" method="post">
+    @csrf
+    <input type="text" name="nome" id="">
+    <input type="password" name="password" id="">
+    <button type="submit">Criar</button>
+    </form>
+</div>
+
 </body>
 </html>
 
