3036138f45e5c0d43be712a19caec51dfb335418
matheusv11 <mathsales360@gmail.com>
2022-12-12 21:48:23 -0300

    Controller de tasks

---
+++ TODOLIST/app/Http/Controllers/taskController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class taskController extends Controller
+{
+
+    
+    static public function getTasks()
+    {
+    $task = new Task();
+    
+    // if(isset(Auth::user()->id)) {
+    //     return $task->where('id_user', Auth::user()->id)->get();
+    // }
+
+    return $task->get();
+
+    // return redirect()->route("tasks.user");
+    
+    }
+
+    public function createTask(Request $request)
+    {
+    $task = new Task();
+
+    if (strlen($request->task)>1) {
+        $task->task = $request->task;
+        $task->id_user = Auth::user()->id;
+        $task->save();
+    }
+    return redirect()->route("tasks.user");
+    }
+
+    public function editTask(Request $request, $taskId)
+    {   
+        $task = Task::find($taskId);
+        $task->completed = $request->checked === 'true' ? 1 : 0;
+        $task->update();
+        return response('Tarefa alterada', 200)->header('Content-Type', 'text/plain');
+    }
+}
